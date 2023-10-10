c4479e2561c6de106c39a348bbdc84b894afa319
mutabazichristian <c.mutabazi@alustudent.com>
2023-10-10 11:52:50 +0200

    now connection is established

--- server/server.js
+++ server/server.js
@@ -1,13 +1,40 @@
-const mysql = require('mysql');
-const express = require('express');
-const bodyparser = require('body-parser');
-var app = express();
+import mysql from 'mysql';
+import express from 'express';
+import bodyParser from 'body-parser';
+import passwordConnect from './passwordConnect.js';
+
+var app = express(mysql);
 const PORT = process.env.PORT || 8080;
-const {authenticate} = require("./authenticate");
+
+console.log(passwordConnect)
+
 
 //configuring express server
-app.use(bodyparser.json());
-authenticate(mysql)
+app.use(bodyParser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: "localhost",
+    user: 'mutabazi',
+    password: passwordConnect,
+    database: 'xpensedb',
+    multipleStatements: true
+});
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('Connection to db Established Successfully');
+    else
+        console.log('Connection to db Failed' + JSON.stringify(err, undefined, 2));
+})
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}..`);
 })
+
+app.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM User', (err,rows,fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err)
+    })
+});
