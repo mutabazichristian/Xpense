21d6536ef9d2a7842386c41a530155fb6da0e40f
mutabazichristian <c.mutabazi@alustudent.com>
2023-10-11 11:15:45 +0200

    stuff

--- server/server.js
+++ server/server.js
@@ -2,39 +2,27 @@ import mysql from 'mysql';
 import express from 'express';
 import bodyParser from 'body-parser';
 import passwordConnect from './passwordConnect.js';
+import config from './webpack.config.js';
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpack from 'webpack';
 
-var app = express(mysql);
 const PORT = process.env.PORT || 8080;
-
-console.log(passwordConnect)
-
-
-//configuring express server
-app.use(bodyParser.json());
+const compiler = webpack(config)
+const app = express();
 
 var mysqlConnection = mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
     user: 'mutabazi',
     password: passwordConnect,
     database: 'xpensedb',
     multipleStatements: true
-});
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('Connection to db Established Successfully');
-    else
-        console.log('Connection to db Failed' + JSON.stringify(err, undefined, 2));
 })
-
+mysqlConnection.connect();
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}..`);
+    console.log(`listening on port ${PORT}`);
 })
 
-app.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM User', (err,rows,fields) => {
-        if (!err)
-            res.send(rows);
-        else
-            console.log(err)
-    })
-});
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+}));
+
