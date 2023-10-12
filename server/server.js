48bbb8f06c2e9cd09497e364ce72778964e6ee27
mutabazichristian <c.mutabazi@alustudent.com>
2023-10-12 11:05:54 +0200

    setting up auth serverside

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import mysql from 'mysql';
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import passwordConnect from './passwordConnect.js';
+
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -14,11 +16,22 @@ var mysqlConnection = mysql.createConnection({
     multipleStatements: true
 })
 mysqlConnection.connect();
+app.use(express.json());
+app.use(cors());
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
 
+app.post('/login',(req,res)=>{
+    const sql = 'SELECT * FROM User WHERE email = ? AND password = ?';
 
-app.get('*', (req,res)=>{
+    mysqlConnection.query(sql,[req.body.email, req.body.password],(err,data)=>{
+        if(err) return res.json('Login Failed');
+        if (data.length > 0){
+            return res.json("Log in successfully");
+        }else{
+            return res.json('no record')
+        }
+    })
 })
-
