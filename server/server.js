98ee884fc45fd96d0bd3e49d6dac1b269265b7f3
mutabazichristian <c.mutabazi@alustudent.com>
2023-10-12 09:23:45 +0200

    yesterday's changes

--- server/server.js
+++ server/server.js
@@ -2,12 +2,8 @@ import mysql from 'mysql';
 import express from 'express';
 import bodyParser from 'body-parser';
 import passwordConnect from './passwordConnect.js';
-import config from './webpack.config.js';
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpack from 'webpack';
 
 const PORT = process.env.PORT || 8080;
-const compiler = webpack(config)
 const app = express();
 
 var mysqlConnection = mysql.createConnection({
@@ -22,7 +18,7 @@ app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
 
-app.use(webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
-}));
+
+app.get('*', (req,res)=>{
+})
 
