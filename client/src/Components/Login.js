337f41ffeb58ec8e7d4f482f17443a1fb3a9b8da
mutabazichristian <c.mutabazi@alustudent.com>
2023-10-12 11:37:37 +0200

    implementing sessions

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -7,7 +7,10 @@ function Login() {
     function handleLogin(event) {
         event.preventDefault();
         axios.post('http://localhost:8080/login', { email, password })
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res);
+                localStorage.setItem("sessiionId",res.data.sessionId);
+            })
             .catch(err => console.log(err));
     }
     return (
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-import mysql from 'mysql';
-import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import mysql from 'mysql';
 import passwordConnect from './passwordConnect.js';
+import { v4 as uuidv4 } from 'uuid';
 
-
-const PORT = process.env.PORT || 8080;
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
@@ -23,15 +23,25 @@ app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
 
-app.post('/login',(req,res)=>{
+app.post('/login', (req, res) => {
     const sql = 'SELECT * FROM User WHERE email = ? AND password = ?';
 
-    mysqlConnection.query(sql,[req.body.email, req.body.password],(err,data)=>{
-        if(err) return res.json('Login Failed');
-        if (data.length > 0){
+    mysqlConnection.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if (err) return res.json('Login Failed');
+        if (data.length > 0) {
+            //create a session Id
+            const sesssionId = generateSessionId();
+            res.json({
+                sesssionId: sesssionId
+            })
             return res.json("Log in successfully");
-        }else{
+        } else {
             return res.json('no record')
         }
     })
 })
+
+
+function generateSessionId() {
+    return uuidv4();
+}
