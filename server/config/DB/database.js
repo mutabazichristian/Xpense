3407cac823c16f7f968e597507db1b1822841bbe
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-09 13:29:56 +0200

    refactoring

--- server/config/DB/database.js
+++ server/config/DB/database.js
@@ -1,20 +1,8 @@
 import { Sequelize } from "sequelize";
 import tokens from "../tokens.js";
-
-const url = `mysql://mutabazi/${tokens.passwordConnect}@locahost:3000/xpensedb`
-
-const sequelizeConnection = new Sequelize(url, {
+const sequelizeConnection = new Sequelize('xpensedb', 'mutabazi', tokens.passwordConnect, {
+    host: 'localhost',
     dialect: 'mysql'
-})
-
-(async ()=>{
-    try {
-        
-    }
-})
+});
 
 export default sequelizeConnection;
-// const sequelizeConnection = new Sequelize('xpensedb', 'mutabazi', tokens.passwordConnect, {
-//     host: 'localhost',
-//     dialect: 'mysql'
-// });
--- server/index.js
+++ server/index.js
@@ -1,3 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+	origin: 'http://localhost:8080/'
+		};
+app.use (cors(corsOptions));
+
+// parse requests of content-type - application/json
+
+app.use (bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+
+app.use (bodyParser.urlencoded({extended:true}));
+
+//simple route
+app.get ('/', (req, res) => {
+res.json({message: 'Welcome to Turing.com'});
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+console.log (`Server is running on port ${PORT}.` );
+});
+
+
 // // import cors from "cors";
 // import express, { query } from "express";
 // import cookieParser from "cookie-parser";
@@ -34,33 +64,3 @@
 // 	console.error(err.stack);
 // 	res.status(500).send('Something went wrong!');
 // });
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-
-var corsOptions = {
-	origin: 'http://localhost:8080/'
-		};
-app.use (cors(corsOptions));
-
-// parse requests of content-type - application/json
-
-app.use (bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-
-app.use (bodyParser.urlencoded({extended:true}));
-
-//simple route
-app.get ('/', (req, res) => {
-res.json({message: 'Welcome to Turing.com'});
-});
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-console.log (`Server is running on port ${PORT}.` );
-});
