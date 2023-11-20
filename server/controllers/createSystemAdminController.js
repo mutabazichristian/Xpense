509942e8c1d07c9b6fb3b71a290fad2429e6341a
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-20 10:50:07 +0200

    mechanism to create system admin

--- server/controllers/createSystemAdminController.js
+++
@@ -1,8 +0,0 @@
-import { SystemAdmin } from '../models/index'
-
-const createSystemAdmin = async (req, res) => {
-    console.log('this is the req', req);
-    console.log(await SystemAdmin.getAll())
-}
-
-module.exports = { createSystemAdmin };
--- server/routes/createSystemAdminRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { createSystemAdmin } from '../controllers/createSystemAdminController';s
-
-const router = express.Router();
-
-router.post('/', createSystemAdmin);
