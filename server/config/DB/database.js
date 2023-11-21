0f2b8eb48b23174648031891342a81793e17eb09
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-21 11:42:23 +0200

    changing to environment variables when setting up the database

--- server/config/DB/database.js
+++ server/config/DB/database.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require('sequelize');
-const tokens = require('../tokens.js');
-const sequelizeConnection = new Sequelize('xpensedb', 'mutabazi', tokens.passwordConnect, {
-    host: 'localhost',
+const database = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const sequelizeConnection = new Sequelize(database, username, password, {
+    host: host,
     dialect: 'mysql'
 });
 
